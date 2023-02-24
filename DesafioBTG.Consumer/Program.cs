139af71645d9cd1874c6a661d8e04d2dc792f10b
gustavo ferreira <gustavofvieira@live.com>
2023-02-23 22:48:25 -0300

    add publisher and consumer rabbitmq

---
+++ DesafioBTG.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            //Port = 8080,
+            Port = 15672,
+            UserName = "user",
+            Password = "password",
+        };
+        using (var connection = factory.CreateConnection())
+
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "requests-queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Recebida : {message}");
+            };
+
+            channel.BasicConsume(queue: "requests-queue", 
+                autoAck: true,
+                consumer: consumer);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ DesafioBTG.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+static class Program { 
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            //Port = 8080,
+            Port = 15672,
+            UserName = "user",
+            Password = "password",
+        };
+        using (var connection = factory.CreateConnection())
+    
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "requests-queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+    
+            string message = "teste da aplicação";
+            var body = Encoding.UTF8.GetBytes(message);
+    
+            channel.BasicPublish(exchange: "",
+                routingKey: "requests-queue",
+                basicProperties: null,
+                body: body);
+    
+            Console.WriteLine($"[x] Enviada: {message}");
+        }
+        Console.ReadLine();
+    }
+}
